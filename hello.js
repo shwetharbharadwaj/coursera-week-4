1766dc3c5cf9278d7099edaaf81497dd82105121
shwetharbharadwaj <46071168+shwetharbharadwaj@users.noreply.github.com>
2020-08-11 15:46:26 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -11,11 +11,4 @@
 
 })(window);
 
-// (function(window) {
-//     var helloSpeaker = new Object();
-//     var speakWord = "Hello";
-//     helloSpeaker.speak = function(name) {
-//         console.log(speakWord + " " + name);
-//     };
-//     window.helloSpeaker = helloSpeaker;
-// })(window);
+
